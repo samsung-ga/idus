34d62f0763b5dfcbe1ad0e47016400d19a7127de
jaeyong <maxwodyd98@gmail.com>
2020-11-14 17:10:09 +0900

    Create init project

---
+++ idus/idus/Extension/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor.swift
+//  idus
+//
+//  Created by 이재용 on 2020/11/14.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    @nonobjc class var reddish: UIColor {
+      return UIColor(red: 190.0 / 255.0, green: 86.0 / 255.0, blue: 71.0 / 255.0, alpha: 1.0)
+    }
+}
---
+++ idus/idus/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  idus
+//
+//  Created by 이재용 on 2020/11/14.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+
+}
+
