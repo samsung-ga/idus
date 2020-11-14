f565b1a11452d110476ff71fd80a9fd10a6e7194
JUEUN KIM <uon9816@naver.com>
2020-11-15 02:10:55 +0900

    20201115

---
+++ idus/idus/Craft/BigImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  BigImageCell.swift
+//  idus
+//
+//  Created by JUEUN KIM on 2020/11/15.
+//
+
+import UIKit
+
+class BigImageCell: UICollectionViewCell {
+    static let identifier = "BigImageCell"
+    
+}
---
+++ idus/idus/Craft/fourBtnCell.swift
@@ -0,0 +1,13 @@
+//
+//  fourBtnCell.swift
+//  idus
+//
+//  Created by JUEUN KIM on 2020/11/15.
+//
+
+import UIKit
+
+class fourBtnCell: UICollectionViewCell {
+    static let identifier = "fourBtnCell"
+    
+}
